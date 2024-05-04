f1dac850f5105644a79012dc2f6a0936566f83ff
vrindaj <vrinda.joshi@hotmail.com>
2024-05-04 15:30:09 -0400

    more

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function displayCode()
-{
-    document.getElementById("code").innerHTML = 'CGSA2';
-}
+function triggerExample1() {
+    // get the container
+    const element = document.querySelector('#example1');
+    // Create a fake `textarea` and set the contents to the text
+    // you want to copy
+    const storage = document.createElement('textarea');
+    storage.value = element.innerHTML;
+    element.appendChild(storage);
+  
+    // Copy the text in the fake `textarea` and remove the `textarea`
+    storage.select();
+    storage.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    element.removeChild(storage);
 
-function displayCode2()
-{
-    document.getElementById("code2").innerHTML = 'DBS3D';
-}
+    alert("Copied the code!");
+  }
